b93113f7d4b65b11e641413865b53a4f8025afce
awhited16 <amw157@case.edu>
2019-08-25 22:54:37 -0400

    Added functionality to update number of guesses, list guest, and update wins and losses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,75 @@
 $(document).ready(function() {
 
-    //List of variables including alphabet array and computer random selection
-    var wins = 0;
-    var losses = 0;
-    var guess = "";
-    var guessesLeft = 9;
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var computerLetter = options[Math.floor(Math.random()*options.length)];
+    function initialize () {
+
+        //List of variables including alphabet array and computer random selection
+        var wins = 0;
+        var losses = 0;
+        var guess = "";
+        var guessesLeft = 9;
+        var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        var computerLetter = options[Math.floor(Math.random()*options.length)];
 
 
-    //Variables that reference HTML
+        //Variables that reference HTML
 
-    var winsText = document.getElementById("win-number");
-    var lossesText = document.getElementById("loss-number");
-    var guessLeftText = document.getElementById("guesses-left");
-    var guessText = document.getElementById("guesses");
+        var winsText = document.getElementById("win-number");
+        var lossesText = document.getElementById("loss-number");
+        var guessLeftText = document.getElementById("guesses-left");
+        var guessText = document.getElementById("guesses");
 
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    guessLeftText.textContent = "Guesses Left: " + guessesLeft;
-    guessText.textContent = "Your Guesses so far: " + guess
-
-    //Have computer select random letter
-
-    console.log(computerLetter);
-    console.log(guess);
-    console.log("wins: " + wins);
-    console.log("losses: " + losses);
-    console.log("guesses left:" + guessesLeft);
-
-    document.onkeyup = (function(event) {
-        var guess = event.key;
-
-        if (guess === computerLetter) {
-            console.log("yes");
-            wins++;
-        } else if (guessesLeft < 9) {
-            console.log("other guess");
-            $(guessText).append(", "+ guessText);
-            guessesLeft--;
-        } else {
-            console.log("first guess");
-            guessesLeft--;
-            $(guessText).append(guessText);
-        }
-
-        //Update game information
         winsText.textContent = "Wins: " + wins;
         lossesText.textContent = "Losses: " + losses;
         guessLeftText.textContent = "Guesses Left: " + guessesLeft;
         guessText.textContent = "Your Guesses so far: " + guess
 
 
+        console.log(computerLetter);
         console.log(guess);
-        console.log("guessesLeft: " + guessesLeft);
-    });
+        console.log("wins: " + wins);
+        console.log("losses: " + losses);
+        console.log("guesses left:" + guessesLeft);
+
+    
+
+        document.onkeyup = (function(event) {
+            var guess = event.key;
+
+            guessText.textContent = "Your Guesses so far: " + guess
+
+            if (guess === computerLetter) {
+                console.log("yes");
+                wins++;
+            } else if (guessesLeft < 9) {
+                console.log("other guess");
+                guessesLeft--;
+                $(guessText).append(", "+ guessText);
+            } else {
+                console.log("first guess");
+                guessesLeft--;
+                $(guessText).append(guessText);
+            }
+
+            //Update game information
+            winsText.textContent = "Wins: " + wins;
+            lossesText.textContent = "Losses: " + losses;
+            guessLeftText.textContent = "Guesses Left: " + guessesLeft;
+            
+
+            console.log(guess);
+            console.log("guessesLeft: " + guessesLeft);
+
+            if (guessesLeft = 0) {
+                losses++;
+                initialize ()
+            }
+
+        });
+
+    
+    }
+
+    initialize ()
 
     //Display wins, losses, and guess info
 
