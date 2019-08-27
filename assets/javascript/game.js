7b975eb9837dc104bfa629a7a217cea382a14a87
awhited16 <amw157@case.edu>
2019-08-26 22:55:00 -0400

    clean up headings, finalize functionality of list of guesses and reset upon win or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,10 +9,11 @@ $(document).ready(function() {
     function initialize () {
 
         //List of variables that restart at the end of each game
-        var guess = [];
+        var guessList = [];
         var guessesLeft = 9;
         var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         var computerLetter = options[Math.floor(Math.random()*options.length)];
+        var guess;
 
 
         //Variables that reference HTML
@@ -25,7 +26,6 @@ $(document).ready(function() {
         winsText.textContent = "Wins: " + wins;
         lossesText.textContent = "Losses: " + losses;
         guessLeftText.textContent = "Guesses Left: " + guessesLeft;
-        guessText.textContent = "Your Guesses so far: ";
 
 
         console.log(computerLetter);
@@ -39,15 +39,13 @@ $(document).ready(function() {
         document.onkeyup = (function(event) {
             var guess = event.key;
 
-            guessText.textContent = "Your Guesses so far: " + guess
-
-
             //add win if guess is correct, restart game
 
             if (guess === computerLetter) {
                 console.log("yes");
                 wins++;
                 guessesLeft = 9;
+                document.getElementById("guesses").innerHTML = "Your Guesses so far: "
                 initialize ();
             }
             
@@ -57,13 +55,17 @@ $(document).ready(function() {
             if (guess !== computerLetter && guessesLeft < 9) {
                 console.log("other guess");
                 guessesLeft--;
-                $("#guesses").append(", "+ guess);
+                console.log(guess);
+                guessList.push(guess);
+                document.getElementById("guesses").innerHTML = "Your Guesses so far: " + guessList;
             }
 
             if (guess !== computerLetter && guessesLeft === 9) {
                 console.log("first guess");
                 guessesLeft = 8;
-                $("#guesses").push(guess);
+                console.log(guess)
+                guessList.push(guess);
+                document.getElementById("guesses").innerHTML = "Your Guesses so far: " + guessList;
             }
 
             
@@ -72,6 +74,7 @@ $(document).ready(function() {
             if (guessesLeft === 0) {
                 losses++;
                 guessesLeft = 9;
+                document.getElementById("guesses").innerHTML = "Your Guesses so far: "
                 initialize ();
             }
 
